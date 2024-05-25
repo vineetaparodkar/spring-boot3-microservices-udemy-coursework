e0d9ccd7f6f9404f29c460b7430f934765ed1707
vineetaparodkar <vineetaparodkar@gmail.com>
2024-05-25 23:22:13 +0530

    Adds config bus module configuration

--- department-service/src/main/java/com/example/departmentservice/configuration/ApplicationProperties.java
+++ department-service/src/main/java/com/example/departmentservice/configuration/ApplicationProperties.java
@@ -1,6 +1,7 @@
 package com.example.departmentservice.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
  * @implNote : One place to access all application properties
  */
 @Configuration
+@RefreshScope
 public class ApplicationProperties {
 
     @Value("${web.config.allowed-origin-urls}")
